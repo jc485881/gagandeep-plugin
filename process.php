c1e90f77e69b4d7ad25fe5a814e2b335dc3016b4
jc485881 <45929304+jc485881@users.noreply.github.com>
2020-01-23 11:04:02 +1000

    process coding

--- process.php
+++ process.php
@@ -17,11 +17,15 @@ function Connect()
 $conn = Connect();
 if(isset($_POST['submit'])){
 $fname = $conn->real_escape_string($_POST['fname']);
-$lname = $conn->real_escape_string($_POST['lname']);
+$phone = $conn->real_escape_string($_POST['phone']);
 $email = $conn->real_escape_string($_POST['email']);
-$bio = $conn->real_escape_string($_POST['bio']); 
+$taxi = $conn->real_escape_string($_POST['taxi']);
+$extras = $conn->real_escape_string($_POST['extras']);
+$pplace = $conn->real_escape_string($_POST['pplace']);
+$dpalce = $conn->real_escape_string($_POST['dpalce']);
+$comments = $conn->real_escape_string($_POST['comments']); 
 
-$query ="INSERT into wp_my_table(fname,lname,email,bio) VALUES ('$fname','$lname','$email','$bio')";
+$query ="INSERT into wp_my_table(fname,phone,email,taxi,extras,pplace,dpalce,comments) VALUES ('$fname','$phone','$email','$taxi,'$extras','$pplace','$dpalce','$comments')";
 
 echo $query;
 $success = $conn->query($query);
